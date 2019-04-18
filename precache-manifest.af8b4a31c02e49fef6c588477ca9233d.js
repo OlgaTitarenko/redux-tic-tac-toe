9e0a7d3341d85283e5e66e927d96519d6fff88fe
титаренко <kizuneworld@gmail.com>
2019-04-18 23:09:02 +0300

    Updates

---
+++ precache-manifest.af8b4a31c02e49fef6c588477ca9233d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddfdc94e04b55bab93de",
+    "url": "/redux-tic-tac-toe/static/js/runtime~main.8464f07e.js"
+  },
+  {
+    "revision": "8ae0450b64d802c241c1",
+    "url": "/redux-tic-tac-toe/static/js/main.d41f5d68.chunk.js"
+  },
+  {
+    "revision": "872470e91cacbe951aa7",
+    "url": "/redux-tic-tac-toe/static/js/2.396e8821.chunk.js"
+  },
+  {
+    "revision": "8ae0450b64d802c241c1",
+    "url": "/redux-tic-tac-toe/static/css/main.5e3296b2.chunk.css"
+  },
+  {
+    "revision": "62d8c5987ebc97552596d5571b51367d",
+    "url": "/redux-tic-tac-toe/index.html"
+  }
+];
